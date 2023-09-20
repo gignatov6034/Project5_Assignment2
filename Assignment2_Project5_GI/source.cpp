6dfbf5e769a49495a5e75503820772b5c15d1aa2
gignatov6034 <gignatov6034@conestogac.on.ca>
2023-09-20 14:40:10 -0400

    Implemented the function that reads from the file, creates student object and store them into a vector space

--- Assignment2_Project5_GI/source.cpp
+++ Assignment2_Project5_GI/source.cpp
@@ -1,6 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
+using namespace std;
 
-int main()
+//This struct keeps information about students: first and last names
+struct studentData {
+    string firstName;
+    string lastName;
+};
+
+int main() 
 {
-	return 1;
+    //open the file 
+    ifstream inFile("Resource Files/StudentData.txt");
+
+    //Check if the file is open
+    if (!inFile) {
+        cerr << "Unable to open file StudentData.txt";
+        return 1; 
+    }
+
+    vector<studentData> studentDataVector;  
+
+    studentData studentData;                //Student struct to temporary keep the info from each line
+
+    string line;                            //Represents a line from a file
+
+    int lineNum = 0; //This keeps the number of the current line in file 
+
+    while (getline(inFile, line)) {
+        lineNum++; //Increase the number by 1 each iteration; 
+        
+        size_t commaPosition = line.find(','); //Find the position of a comma that separates the first and last names
+
+        if (commaPosition != string::npos) //Check if no comma is present in the line
+        {
+            if (lineNum > 1)
+            {
+                studentData.firstName = line.substr(0, commaPosition);
+                studentData.lastName = line.substr(commaPosition + 1);
+
+                studentDataVector.push_back(studentData);
+            }
+        }
+    }
+
+    //lineNum = 0; //reset the lineNum
+
+    //Close the file
+    inFile.close();
+
+    return 0; 
 }
